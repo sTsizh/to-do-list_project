bf7ff1ba1b88e8fcb6f3829545d2b4da01d305a1
sTsizh <sofiatsizh@gmail.com>
2025-08-13 21:19:20 +0300

    Update main.js

--- main.js
+++ main.js
@@ -8,30 +8,25 @@ const newTaskInput = document.getElementById('newTaskInput'); // input field to 
 const addTaskButton = document.getElementById('addTaskBtn') //Add task button
 const taskList = document.getElementById('taskList'); // list of tasks
 
-let listsAndTasksArr = [] //array to store all lists and its tasks in local storage
-let regExp = /^\s*$/ //regExp to exclude empty input fields
+let listsAndTasksArr = []   //array to store all lists and its tasks in local storage
+let regExp = /^\s*$/   //regExp to exclude empty input fields
 
 function addList(){
-    //check the empty input field
     let listName = newListInput.value
     if (regExp.test(listName)){
         inputField = ''
         return
     }
-    //display each list
     createListHTML(listName)
-    //store each list and its tasks as an object in array
     let listAndTasksObj = {
         list: listName,
         tasks: [],
     }
     listsAndTasksArr.push(listAndTasksObj)
-    //clear the field
     newListInput.value = ''
     saveToLocalStorage()
 }
 
-//display each list on webpage
 function createListHTML(listName){
         let newList = document.createElement('div')
         newList.className = 'list-item'
@@ -42,40 +37,32 @@ function createListHTML(listName){
 function removeList(){
     let activeList = document.getElementsByClassName('list-item active')[0]
     activeList.remove()
-    //remove the tasks content
     listTitle.innerHTML = 'Select a list'
     taskInputSection.style = "display:none"
     taskList.style = "display:none"
-    //remove list from array (of lists and tasts) that is stored in local storage
     listsAndTasksArr = listsAndTasksArr.filter(elem => elem.list != activeList.textContent)
     saveToLocalStorage()
 }
 
 function selectList(event){
     event.preventDefault()
-    //make all lists - inactive
     let activeLists = document.getElementsByClassName('list-item active')
     for(let list of activeLists){
         list.className = 'list-item'
     }
-    //make selected list - active
     event.target.className = 'list-item active'
-    //show the tasks content
     taskInputSection.style = "display:yes"
     taskList.style = "display:yes"
     listTitle.innerHTML = event.target.innerText
-
     renderTasks()
     saveToLocalStorage()
 } 
 
 function addTask(){
-    //check the empty input field
     if (regExp.test(newTaskInput.value)){
         inputField = ''
         return
     }
-    //each task is stored as an object with taskName and completed atribute
     let selectedList = document.getElementsByClassName('list-item active')[0].innerHTML
     for(let el of listsAndTasksArr){
         if(el.list == selectedList){
@@ -84,32 +71,25 @@ function addTask(){
                 })
             }
     }
-    //display tasks
     renderTasks()
-    //clear the input field
     newTaskInput.value = ''
     saveToLocalStorage()
 }
 
-//display each task on webpage
 function createTaskHTML(taskObj){
         let newTask = document.createElement('li')
-
         if(taskObj.completed){
             newTask.className = 'task-text completed'
         } else{
             newTask.className = 'task-text'
         }
-        
         newTask.innerHTML = taskObj.taskName
         taskList.append(newTask)
-        //each task has delete button
         const delButton = document.createElement('button')
         delButton.textContent = 'X';
         newTask.append(delButton)
 }
 
-//display current Tasks for selected list
 function renderTasks() {
     taskList.innerHTML = '';
     let selectedList = document.getElementsByClassName('list-item active')[0].innerHTML
@@ -142,7 +122,6 @@ function markAsDone(event){
     saveToLocalStorage()
 }
 
-//update array of listsAndTasks with corrects tasks list
 function updateTasksList(){
     let currentTasksArr = document.getElementsByClassName('task-text')
     let updatedTasksArr = []
@@ -157,7 +136,6 @@ function updateTasksList(){
             })
         }
     }
-
     let selectedList = document.getElementsByClassName('list-item active')[0].innerHTML
     for(let el of listsAndTasksArr){
         if(el.list == selectedList){
@@ -175,7 +153,6 @@ function showDataFromLocalStorage() {
     if(!listsAndTasks){
         return []
     }
-    //display lists on webpage
     for(let el of listsAndTasks){
         createListHTML(el.list)
     }
